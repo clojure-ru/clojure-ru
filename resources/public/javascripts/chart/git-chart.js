57eeb847d16f58b0d6317e5eff738df83edde977
Евгений <elizavetaskull@gmail.com>
2014-12-17 02:59:43 +0600

    table for legend

--- resources/public/javascripts/chart/git-chart.js
+++ resources/public/javascripts/chart/git-chart.js
@@ -1,6 +1,7 @@
 var canvasID = "libraries-stat",
 	gitChart = null,
-	lineColors = ["#C8001A", "#2F3871", "#FF5022", "#FFAF00", "#5083F1", "#002157", "#0076A3", "#BD8CBF", "#603913", "#C69C6D"]
+	lineColors = ["#C8001A", "#2F3871", "#FF5022", "#FFAF00", "#5083F1", "#002157", "#0076A3", "#BD8CBF", "#603913", "#C69C6D"],
+	columnNames = ['&nbsp;&nbsp;', 'Имя репозитория', 'Индекс', 'Коммитов за неделю'];
 
 function makeChart(repositories){
 	'use strict'
@@ -23,20 +24,67 @@ function makeChart(repositories){
 		data.push(line);
 	});
 
-	var legendStr = "<ul class=\"line-legend\">";
-	
-	for (var i=0; i<data.length; i++){
-		legendStr += "<li><span style=\"background-color:" + data[i].strokeColor + "\">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;";
-		legendStr += "<a href=\"" + data[i].url + "\">" + data[i].label + "</a>";
-		legendStr += "<div class=\"weights\">" + Math.round(repositories[i].cplx) + "</div>";
-		legendStr += "<div class=\"weights\">" + Math.round(repositories[i].diffw) + "</div>";
-		legendStr += "<div class=\"weights\">" + Math.round(repositories[i].w) + "</div>";
-		legendStr += "<div class=\"weights\">" + Math.round(repositories[i].sum) + "</div>";
-		legendStr += "</li>";
-	}
-	legendStr += "</ul>";
+	var GitLegend = function(colNames, data){
+		var self = {};
+		self.colNames = colNames;
+		self.data = data;
 
-	document.getElementById(canvasID).parentNode.innerHTML += legendStr;
+		self.build = function(){
+			var out = document.createElement("table"),
+				currentRow = null,
+				addRow = function(){
+					currentRow = document.createElement("tr");
+					out.appendChild(currentRow);
+				},
+				wrap = function(name, node){
+					var newNode = document.createElement(name);
+					newNode.appendChild(node);
+					return newNode;
+				};
+
+			out.classList.add("line-legend");
+
+			addRow();
+
+			self.colNames.forEach(function (val){
+				var th = document.createElement('th');
+				th.innerHTML = val;
+				currentRow.appendChild(th);
+			});
+
+			self.data.forEach(function(val, idx){
+				addRow();
+				var color = document.createElement('div'),
+					name = document.createElement('td'),
+					weight = document.createElement('td'),
+					increment = document.createElement('td');
+
+				color.style = "background-color:" + val.strokeColor;
+				name.innerHTML = '<a href="' + val.url + '">' + val.label + '</a>';
+				weight.innerHTML = Math.round(repositories[idx].w);
+				increment.innerHTML = Math.round(repositories[idx].sum);
+
+				color.classList.add('lineColor');
+				weight.classList.add('number');
+				increment.classList.add('number');
+
+				currentRow.appendChild(wrap('td', color));
+				currentRow.appendChild(name);
+				currentRow.appendChild(weight);
+				currentRow.appendChild(increment);
+			});
+
+			return out;
+		}
+
+		self.buildHTML = function() { return self.build().outerHTML; }
+
+		return self;
+	}
+
+	document.getElementById(canvasID).parentNode.appendChild(
+		GitLegend(columnNames,data).build()
+	);
 
 	var steps = Math.ceil(maxIncrement/20),
 		gitChart = new Chart(document.getElementById(canvasID).getContext("2d")).Line(
