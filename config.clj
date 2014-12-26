a37f678fafab144c462d98b341a42ed79e1bbd99
Vlad Bokov <razum2um@mail.ru>
2014-12-26 17:18:08 +0600

    update config

--- config.clj
+++ config.clj
@@ -1,10 +1,10 @@
-[:site-title "Clojure.RU" ; The default title of the pages
+[:site-title "clojure.ru" ; The default title of the pages
  :site-title-page " - Page %s" ; The extension for older pages
  :site-title-tag " - Tag %s" ; The extension for tags
- :site-description "Documenting Clojure research"
- :site-url "http://razum2um.me"
- :site-default-keywords "clojure javascript clojurescript html ios mac research"
- :site-author "Vladimir Bokov (Vlad) and Evgenij Solovjev"
+ :site-description "Русскоязычное clojure сообщество"
+ :site-url "http://clojure.ru"
+ :site-default-keywords "clojure clojurescript"
+ :site-author "Vladimir Bokov, Evgenij Solovjev"
  :in-dir "resources/"
  :out-dir "html/"
  :post-dir "news/"
@@ -15,7 +15,7 @@
  :blog-as-index false
  :posts-per-page 5
  :create-archives true
- :archives-title "Recent News"
+ :archives-title "Последние записи"
  :archives-title-month " - %s" ; for the individual month
  :date-format-post "dd/MM/yyyy" ; the output dateformat for posts
  :date-format-rss "E, d MMM yyyy HH:mm:ss Z" ; the date format for the rss feed
